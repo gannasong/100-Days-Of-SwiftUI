193e035e15aafde6a4edb4645b2a5eccada2393f
Miguel <gannasong@gmail.com>
2022-07-31 10:32:05 +0800

    feat: complete day21 project2, part2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by SUNG HAO LIN on 2022/7/30.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
