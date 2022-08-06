222d82d29477af0f3e716a3f27ed336bb5347d34
Miguel <gannasong@gmail.com>
2022-08-06 10:35:34 +0800

    feat: implement day27 project4, part2

---
+++ BetterRest - Project4/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by SUNG HAO LIN on 2022/8/5.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
